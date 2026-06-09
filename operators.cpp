32f53d819b839e518d6f778a4ecf2bc9b7b9dd7d
DishaDama4 <dishadama4@gmail.com>
2026-06-09 22:38:10 +0530

    operators

---
+++ operators.cpp
@@ -0,0 +1,15 @@
+//  Operator :-  i. Arithmetic operators :-  + , - , * , / , % 
+//               ii. Relational :- = , == , != , < , > , <= , >=
+//               iii. Logical :- && , || 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int  a=2 , b=4;
+    cout<<"a + b = "<<a+b;
+    cout<<"\n a - b = "<<a-b;
+    cout<<"\n a * b = "<<a*b;
+    cout<<"\n a / b = "<<a/b;
+    cout<<"\n a % b = "<<a%b;
+    return 0;
+}

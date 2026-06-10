41797ae265d6cb45b223e5567b2e9c1f9be1f253
DishaDama4 <dishadama4@gmail.com>
2026-06-10 21:54:15 +0530

    if-else statements

---
+++ condition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a number :";
+    cin>>a;
+    if(a>=0)
+    {
+        cout<<"\n Positive number";
+    }
+    else{
+        cout<<"\n Negitive number";
+    }
+    return 0;
+}

2908b8ec8d95a535c51fb10ebf0577a0402a8815
DishaDama4 <dishadama4@gmail.com>
2026-06-10 22:00:17 +0530

    Delete first.cpp

--- first.cpp
+++
@@ -1,9 +0,0 @@
-  #include<iostream>
- using namespace std; 
- int main(){
-    // cout<< "Hello World !";
-    int m =6 , n=9;
-    cout<< "m ="<<m <<" n= "<<n;
-    cout<< " \n m+n = "<<m+n;
-    return 0;
- }
